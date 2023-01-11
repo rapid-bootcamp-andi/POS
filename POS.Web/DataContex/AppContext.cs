afd722af8a6bb2d26db7c9b6593001c3cdb2735d
Andi Dwi Angga Prastya <114982520+SyrMtng@users.noreply.github.com>
2023-01-11 13:41:17 +0700

    CheckPoint12.00

---
+++ POS.Web/DataContex/AppContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POS.Repository
+{
+    public class AppContext : DbContext
+    {
+        public AppContext(DbContextOptions<AppContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> CategoryEntities => Set<Category>();
+        public DbSet<Customer> CustomerEntities => Set<Customer>();
+        public DbSet<Employee> EmployeeEntities => Set<Employee>();
+        public DbSet<Order> OrderEntities => Set<Order>();
+        public DbSet<OrderDetail> OrderDetailEntities => Set<OrderDetail>();
+        public DbSet<Product> ProductEntities => Set<Product>();
+        public DbSet<Supplier> SupplierEntities => Set<Supplier>();
+    }
+}
